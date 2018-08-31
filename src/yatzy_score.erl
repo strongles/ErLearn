a77c8fe82734398cb723ae0d3b7a03d5bcd27b88
Rob Williams <rob.williams@alertlogic.com>
2018-08-31 10:05:15 +0100

    Added specs and unit tests for hand scores

--- src/yatzy_score.erl
+++ src/yatzy_score.erl
@@ -10,7 +10,7 @@
 -author("rob.williams").
 
 %% API
--export([upper/2, one_pair/1, two_pair/1, three_of_a_kind/1, four_of_a_kind/1, full_house/1, small_straight/1, large_straight/1, chance/1, yatzy/1, consecutive_dice/2]).
+-export([upper/2, one_pair/1, two_pair/1, three_of_a_kind/1, four_of_a_kind/1, full_house/1, small_straight/1, large_straight/1, chance/1, yatzy/1]).
 
 -spec upper(integer(), list()) -> integer().
 upper(N, DiceList) ->
@@ -26,70 +26,94 @@ upper(N, [H | T], Acc) ->
     end,
   upper(N, T, Sum).
 
--spec consecutive_dice(integer(), list()) -> {boolean(), integer()} | boolean.
-consecutive_dice(N, DiceList) ->
-  SortedDice = lists:reverse(lists:sort(DiceList)),
-  consecutive_dice(N, SortedDice, 0, 6).
-consecutive_dice(N, [H|_], N, Number) when H =/= Number ->
-  {true, Number};
-consecutive_dice(N, [], N, Number) ->
-  {true, Number};
-consecutive_dice(_, [], _, _) ->
-  false;
-consecutive_dice(N, [H|T], Count, Number) ->
-  if
-    H =:= Number ->
-      consecutive_dice(N, T, Count+1, Number);
-    H =/= Number ->
-      consecutive_dice(N, T, 1, H)
-  end.
+%%-spec consecutive_dice(integer(), list()) -> {boolean(), integer()} | boolean.
+%%consecutive_dice(N, DiceList) ->
+%%  SortedDice = lists:reverse(lists:sort(DiceList)),
+%%  consecutive_dice(N, SortedDice, 0, 6).
+%%consecutive_dice(N, [H|_], N, Number) when H =/= Number ->
+%%  {true, Number};
+%%consecutive_dice(N, [], N, Number) ->
+%%  {true, Number};
+%%consecutive_dice(_, [], _, _) ->
+%%  false;
+%%consecutive_dice(N, [H|T], Count, Number) ->
+%%  if
+%%    H =:= Number ->
+%%      consecutive_dice(N, T, Count+1, Number);
+%%    H =/= Number ->
+%%      consecutive_dice(N, T, 1, H)
+%%  end.
 
--spec score_consecutive(integer(), list()) -> integer().
-score_consecutive(Occurrences, DiceList) ->
-  case consecutive_dice(Occurrences, DiceList) of
-    false ->
-      0;
-    {true, N} ->
-      Occurrences * N
-  end.
+%%-spec score_consecutive(integer(), list()) -> integer().
+%%score_consecutive(Occurrences, DiceList) ->
+%%  case consecutive_dice(Occurrences, DiceList) of
+%%    false ->
+%%      0;
+%%    {true, N} ->
+%%      Occurrences * N
+%%  end.
 
--spec remove_all_occurrences(integer(), list()) -> list().
-remove_all_occurrences(N, DiceList) ->
-  ListLength = length(DiceList),
-  TrimmedList = lists:delete(N, DiceList),
-  case length(TrimmedList) of
-    ListLength ->
-      TrimmedList;
-    _ ->
-      remove_all_occurrences(N, TrimmedList)
-  end.
+%%-spec remove_all_occurrences(integer(), list()) -> list().
+%%remove_all_occurrences(N, DiceList) ->
+%%  ListLength = length(DiceList),
+%%  TrimmedList = lists:delete(N, DiceList),
+%%  case length(TrimmedList) of
+%%    ListLength ->
+%%      TrimmedList;
+%%    _ ->
+%%      remove_all_occurrences(N, TrimmedList)
+%%  end.
 
 -spec one_pair(list()) -> integer().
 one_pair(DiceList) ->
-  score_consecutive(2, DiceList).
+  case lists:reverse(lists:sort(DiceList)) of
+    [N, N, _, _, _] ->
+      N * 2;
+    [_, N, N, _, _] ->
+      N * 2;
+    [_, _, N, N, _] ->
+      N * 2;
+    [_, _, _, N, N] ->
+      N * 2;
+    _ ->
+      0
+  end.
 
 -spec three_of_a_kind(list()) -> integer().
 three_of_a_kind(DiceList) ->
-  score_consecutive(3, DiceList).
+  case lists:sort(DiceList) of
+    [N, N, N, _, _] ->
+      N * 3;
+    [_, N, N, N, _] ->
+      N * 3;
+    [_, _, N, N, N] ->
+      N * 3;
+    _ ->
+      0
+  end.
 
 -spec four_of_a_kind(list()) -> integer().
 four_of_a_kind(DiceList) ->
-  score_consecutive(4, DiceList).
+  case lists:sort(DiceList) of
+    [N, N, N, N, _] ->
+      4 * N;
+    [_, N, N, N, N] ->
+      4 * N;
+    _ ->
+      0
+  end.
 
 -spec two_pair(list()) -> integer().
 two_pair(DiceList) ->
-  case score_consecutive(2, DiceList) of
-    false ->
-      0;
-    {true, N} ->
-      PairScore = 2 * N,
-      PrunedList = remove_all_occurrences(N, DiceList),
-      case score_consecutive(2, PrunedList) of
-        false ->
-          0;
-        {true, Y} ->
-          PairScore + 2 * Y
-      end
+  case lists:sort(DiceList) of
+    [A, A, B, B, _] ->
+      A * 2 + B * 2;
+    [A, A, _, B, B] ->
+      A * 2 + B * 2;
+    [_, A, A, B, B] ->
+      A * 2 + B * 2;
+    _ ->
+      0
   end.
 
 -spec small_straight(list()) -> integer().
@@ -112,17 +136,13 @@ large_straight(DiceList) ->
 
 -spec full_house(list()) -> integer().
 full_house(DiceList) ->
-  case consecutive_dice(3, DiceList) of
-    false ->
-      0;
-    {true, TripleNumber} ->
-      PrunedList = remove_all_occurrences(TripleNumber, DiceList),
-      case consecutive_dice(2, PrunedList) of
-        false ->
-          0;
-        {true, DoubleNumber} ->
-          3 * TripleNumber + 2 * DoubleNumber
-      end
+  case lists:sort(DiceList) of
+    [A, A, A, B, B] ->
+      lists:sum(DiceList);
+    [A, A, B, B, B] ->
+      lists:sum(DiceList);
+    _ ->
+      0
   end.
 
 -spec chance(list()) -> integer().
@@ -130,11 +150,54 @@ chance(DiceList) ->
   lists:sum(DiceList).
 
 -spec yatzy(list()) -> integer().
-yatzy(DiceList) ->
-  case consecutive_dice(5, DiceList) of
-    false ->
-      0;
-    {true, _} ->
-      50
-  end.
+yatzy([N, N, N, N, N]) ->
+  50;
+yatzy(_) ->
+  0.
 
+%% First implementation
+%%yatzy(DiceList) ->
+%%  case consecutive_dice(5, DiceList) of
+%%    false ->
+%%      0;
+%%    {true, _} ->
+%%      50
+%%  end.
+%%
+%%full_house(DiceList) ->
+%%  case consecutive_dice(3, DiceList) of
+%%    false ->
+%%      0;
+%%    {true, TripleNumber} ->
+%%      PrunedList = remove_all_occurrences(TripleNumber, DiceList),
+%%      case consecutive_dice(2, PrunedList) of
+%%        false ->
+%%          0;
+%%        {true, DoubleNumber} ->
+%%          3 * TripleNumber + 2 * DoubleNumber
+%%      end
+%%  end.
+%%
+%%two_pair(DiceList) ->
+%%  case consecutive_dice(2, DiceList) of
+%%    false ->
+%%      0;
+%%    {true, N} ->
+%%      PairScore = 2 * N,
+%%      PrunedList = remove_all_occurrences(N, DiceList),
+%%      case consecutive_dice(2, PrunedList) of
+%%        false ->
+%%          0;
+%%        {true, Y} ->
+%%          PairScore + 2 * Y
+%%      end
+%%  end.
+%%
+%%one_pair(DiceList) ->
+%%  score_consecutive(2, DiceList).
+%%
+%%three_of_a_kind(DiceList) ->
+%%  score_consecutive(3, DiceList).
+%%
+%%four_of_a_kind(DiceList) ->
+%%  score_consecutive(4, DiceList).
---
+++ src/yatzy_score_SUITE.erl
@@ -0,0 +1,118 @@
+%%%-------------------------------------------------------------------
+%%% @author rob.williams
+%%% @copyright (C) 2018, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 28. Aug 2018 12:06
+%%%-------------------------------------------------------------------
+-module(yatzy_score_SUITE).
+-include_lib("common_test/include/ct.hrl").
+-author("rob.williams").
+
+%% API
+-export([all/0]).
+-export([test_one_pair_scored_correctly/1, test_three_of_a_kind_scored_correctly/1,
+  test_four_of_a_kind_scored_correctly/1, test_two_pair_scored_correctly/1, test_full_house_scored_correctly/1,
+  test_yatzy_scored_correctly/1, test_small_straight_scored_correctly/1, test_large_straight_scored_correctly/1,
+  test_chance_scored_correctly/1, test_upper_scored_correctly/1]).
+
+all() -> [test_one_pair_scored_correctly, test_three_of_a_kind_scored_correctly, test_four_of_a_kind_scored_correctly,
+  test_two_pair_scored_correctly, test_full_house_scored_correctly, test_yatzy_scored_correctly,
+  test_small_straight_scored_correctly, test_large_straight_scored_correctly, test_chance_scored_correctly,
+  test_upper_scored_correctly].
+
+
+check_expected_scores_match_actual(TestFunction, TestCase) ->
+  ResultList = lists:map(TestFunction, [TestData || {_, TestData} <- TestCase]),
+  case lists:all(fun({X, Y}) -> X =:= Y end, lists:zip(ResultList, [Expected || {Expected, _} <- TestCase])) of
+    true ->
+      ok;
+    false ->
+      erlang:error(badmatch)
+  end.
+
+test_upper_scored_correctly(_Config) ->
+  0 = yatzy_score:upper(5, [1, 1, 1, 1, 1]),
+  4 = yatzy_score:upper(2, [2, 3, 3, 1, 2]),
+  3 = yatzy_score:upper(3, [3, 4, 1, 4, 2]),
+  12 = yatzy_score:upper(4, [4, 5, 6, 4, 4]).
+
+test_one_pair_scored_correctly(_Config) ->
+  2 = yatzy_score:one_pair([1, 1, 6, 3, 2]),
+  4 = yatzy_score:one_pair([1, 2, 3, 2, 5]),
+  6 = yatzy_score:one_pair([3, 1, 3, 4, 5]),
+  8 = yatzy_score:one_pair([4, 1, 1, 2, 4]),
+  10 = yatzy_score:one_pair([1, 4, 2, 5, 5]),
+  12 = yatzy_score:one_pair([5, 6, 3, 6, 2]),
+  0 = yatzy_score:one_pair([1, 2, 3, 4, 6]).
+
+test_three_of_a_kind_scored_correctly(_Config) ->
+  3 = yatzy_score:three_of_a_kind([1, 1, 6, 3, 1]),
+  6 = yatzy_score:three_of_a_kind([1, 2, 3, 2, 2]),
+  9 = yatzy_score:three_of_a_kind([3, 1, 3, 3, 5]),
+  12 = yatzy_score:three_of_a_kind([4, 1, 1, 4, 4]),
+  15 = yatzy_score:three_of_a_kind([5, 4, 2, 5, 5]),
+  18 = yatzy_score:three_of_a_kind([5, 6, 6, 6, 2]),
+  0 = yatzy_score:three_of_a_kind([1, 2, 3, 4, 6]).
+
+test_four_of_a_kind_scored_correctly(_Config) ->
+  4 = yatzy_score:four_of_a_kind([1, 1, 6, 1, 1]),
+  8 = yatzy_score:four_of_a_kind([1, 2, 2, 2, 2]),
+  12 = yatzy_score:four_of_a_kind([3, 3, 3, 3, 5]),
+  16 = yatzy_score:four_of_a_kind([4, 1, 4, 4, 4]),
+  20 = yatzy_score:four_of_a_kind([5, 5, 5, 3, 5]),
+  24 = yatzy_score:four_of_a_kind([5, 6, 6, 6, 6]),
+  0 = yatzy_score:four_of_a_kind([1, 2, 3, 4, 6]).
+
+test_two_pair_scored_correctly(_Config) ->
+  6 = yatzy_score:two_pair([1, 1, 6, 2, 2]),
+  10 = yatzy_score:two_pair([1, 2, 3, 2, 3]),
+  16 = yatzy_score:two_pair([1, 3, 3, 5, 5]),
+  10 = yatzy_score:two_pair([4, 1, 4, 1, 2]),
+  14 = yatzy_score:two_pair([5, 2, 2, 3, 5]),
+  22 = yatzy_score:two_pair([5, 6, 2, 6, 5]),
+  0 = yatzy_score:two_pair([1, 2, 3, 4, 6]).
+
+test_full_house_scored_correctly(_Config) ->
+  8 = yatzy_score:full_house([1, 1, 2, 2, 2]),
+  13 = yatzy_score:full_house([3, 2, 3, 2, 3]),
+  21 = yatzy_score:full_house([5, 3, 3, 5, 5]),
+  11 = yatzy_score:full_house([4, 1, 4, 1, 1]),
+  16 = yatzy_score:full_house([5, 2, 2, 2, 5]),
+  27 = yatzy_score:full_house([5, 6, 5, 6, 5]),
+  0 = yatzy_score:full_house([1, 2, 3, 4, 6]).
+
+test_small_straight_scored_correctly(_Config) ->
+  0 = yatzy_score:small_straight([1, 1, 2, 2, 2]),
+  15 = yatzy_score:small_straight([1, 2, 3, 4, 5]),
+  0 = yatzy_score:small_straight([5, 3, 3, 5, 5]),
+  0 = yatzy_score:small_straight([4, 1, 4, 1, 1]),
+  0 = yatzy_score:small_straight([5, 2, 2, 2, 5]),
+  0 = yatzy_score:small_straight([5, 6, 5, 6, 5]),
+  0 = yatzy_score:small_straight([1, 2, 3, 4, 6]).
+
+test_large_straight_scored_correctly(_Config) ->
+  0 = yatzy_score:large_straight([1, 1, 2, 2, 2]),
+  0 = yatzy_score:large_straight([3, 2, 3, 2, 3]),
+  0 = yatzy_score:large_straight([5, 3, 3, 5, 5]),
+  0 = yatzy_score:large_straight([4, 1, 4, 1, 1]),
+  0 = yatzy_score:large_straight([5, 2, 2, 2, 5]),
+  20 = yatzy_score:large_straight([2, 3, 4, 5, 6]),
+  0 = yatzy_score:large_straight([1, 2, 3, 4, 6]).
+
+test_chance_scored_correctly(_Config) ->
+  16 = yatzy_score:chance([1, 6, 2, 3, 4]),
+  15 = yatzy_score:chance([3, 2, 4, 1, 5]),
+  15 = yatzy_score:chance([5, 3, 1, 2, 4]),
+  16 = yatzy_score:chance([4, 1, 6, 3, 2]),
+  20 = yatzy_score:chance([5, 6, 4, 2, 3]).
+
+test_yatzy_scored_correctly(_Config) ->
+  0 = yatzy_score:yatzy([1, 1, 2, 2, 2]),
+  0 = yatzy_score:yatzy([3, 2, 3, 2, 3]),
+  50 = yatzy_score:yatzy([5, 5, 5, 5, 5]),
+  0 = yatzy_score:yatzy([4, 1, 4, 1, 1]),
+  50 = yatzy_score:yatzy([2, 2, 2, 2, 2]),
+  0 = yatzy_score:yatzy([5, 6, 5, 6, 5]),
+  0 = yatzy_score:yatzy([1, 2, 3, 4, 6]).
