2bc81ccaab0da06d0dc6caa4cb7df3f9643f2073
Rob Williams <rob.williams@alertlogic.com>
2018-09-05 11:57:21 +0100

    Converted indent size from two to four spaces

--- src/yatzy_player.erl
+++ src/yatzy_player.erl
@@ -13,7 +13,7 @@
 -export([new_player/1, record_score/3, final_score/1, print_score/1]). %% roll_dice/1, re_roll/2,
 
 loop(Sheet) ->
-  receive
+    receive
 %%    {From, roll_dice} ->
 %%      DiceList = generate_dice(5),
 %%      NewState = maps:put(dice_hand, DiceList, GameState),
@@ -44,46 +44,46 @@ loop(Sheet) ->
 %%              loop(Sheet, GameState)
 %%          end
 %%      end;
-    {From, {record_score, Hand, DiceList}} ->
-      case yatzy_sheet:fill(Hand, DiceList, Sheet) of
-        {ok, ScoredSheet} ->
-          From ! {ok, yatzy_sheet:get_score(Hand, ScoredSheet)},
-          loop(ScoredSheet);
-        already_filled ->
-          From ! {error, already_filled},
-          loop(Sheet)
-      end;
-    {From, print_score} ->
-      print_score_list([[Hand, Score]|| {Hand, Score} <- maps:to_list(Sheet)]),
-      From ! ok,
-      loop(Sheet);
-    {From, code_change} ->
-      From ! ok,
-      ?MODULE:loop(Sheet);
-    {From, final_score} ->
-      From ! {ok, yatzy_sheet:get_score(total, Sheet)}
-  end.
+        {From, {record_score, Hand, DiceList}} ->
+            case yatzy_sheet:fill(Hand, DiceList, Sheet) of
+                {ok, ScoredSheet} ->
+                    From ! {ok, yatzy_sheet:get_score(Hand, ScoredSheet)},
+                    loop(ScoredSheet);
+                already_filled ->
+                    From ! {error, already_filled},
+                    loop(Sheet)
+            end;
+        {From, print_score} ->
+            print_score_list([[Hand, Score] || {Hand, Score} <- maps:to_list(Sheet)]),
+            From ! ok,
+            loop(Sheet);
+        {From, code_change} ->
+            From ! ok,
+            ?MODULE:loop(Sheet);
+        {From, final_score} ->
+            From ! {ok, yatzy_sheet:get_score(total, Sheet)}
+    end.
 
 print_score_list([]) ->
-  ok;
-print_score_list([H|T]) ->
-  io:format("~p: ~p~n", H),
-  print_score_list(T).
+    ok;
+print_score_list([H | T]) ->
+    io:format("~p: ~p~n", H),
+    print_score_list(T).
 
 -spec new_player(atom()) -> {ok, pid()}.
 new_player(Name) ->
-  Pid = spawn(fun() -> loop(yatzy_sheet:new()) end),
-  register(Name, Pid),
-  {ok, Pid}.
+    Pid = spawn(fun() -> loop(yatzy_sheet:new()) end),
+    register(Name, Pid),
+    {ok, Pid}.
 
 call(Pid, Args) ->
-  Pid ! Args,
-  receive
-    Res ->
-      Res
-  after 4000 ->
-    timeout
-  end.
+    Pid ! Args,
+    receive
+        Res ->
+            Res
+    after 4000 ->
+        timeout
+    end.
 
 %%roll_dice(Name) ->
 %%  {ok, DiceList} = call(Name, {self(), roll_dice}),
@@ -102,18 +102,18 @@ call(Pid, Args) ->
 %%  end.
 
 record_score(Name, Hand, DiceList) ->
-  case call(Name, {self(), {record_score, Hand, DiceList}}) of
-    {ok, Score} ->
-      io:format("~p added ~p for ~p points~n", [Name, Hand, Score]);
-    {error, multi_score} ->
-      io:format("~p is a filthy cheater! You cannot add more than one score per round!~n", [Name]);
-    {error, already_filled} ->
-      io:format("Score already filled for ~p~n", [Hand])
-  end.
+    case call(Name, {self(), {record_score, Hand, DiceList}}) of
+        {ok, Score} ->
+            io:format("~p added ~p for ~p points~n", [Name, Hand, Score]);
+        {error, multi_score} ->
+            io:format("~p is a filthy cheater! You cannot add more than one score per round!~n", [Name]);
+        {error, already_filled} ->
+            io:format("Score already filled for ~p~n", [Hand])
+    end.
 
 final_score(Name) ->
-  {ok, Score} = call(Name, {self(), final_score}),
-  io:format("~p's final score: ~p~n", [Name, Score]).
+    {ok, Score} = call(Name, {self(), final_score}),
+    io:format("~p's final score: ~p~n", [Name, Score]).
 
 print_score(Name) ->
-  ok = call(Name, {self(), print_score}).
+    ok = call(Name, {self(), print_score}).
--- src/yatzy_score.erl
+++ src/yatzy_score.erl
@@ -11,131 +11,131 @@
 
 %% API
 -export([upper/2, one_pair/1, two_pair/1, three_of_a_kind/1, four_of_a_kind/1, full_house/1, small_straight/1,
-  large_straight/1, chance/1, yatzy/1, score_dice/2]).
+    large_straight/1, chance/1, yatzy/1, score_dice/2]).
 
 -spec upper(integer(), list()) -> integer().
 upper(N, DiceList) ->
-  lists:sum(lists:filter(fun(X) -> X =:= N end, DiceList)).
+    lists:sum(lists:filter(fun(X) -> X =:= N end, DiceList)).
 
 -spec one_pair(list()) -> integer().
 one_pair(DiceList) ->
-  case lists:reverse(lists:sort(DiceList)) of
-    [N, N, _, _, _] ->
-      N * 2;
-    [_, N, N, _, _] ->
-      N * 2;
-    [_, _, N, N, _] ->
-      N * 2;
-    [_, _, _, N, N] ->
-      N * 2;
-    _ ->
-      0
-  end.
+    case lists:reverse(lists:sort(DiceList)) of
+        [N, N, _, _, _] ->
+            N * 2;
+        [_, N, N, _, _] ->
+            N * 2;
+        [_, _, N, N, _] ->
+            N * 2;
+        [_, _, _, N, N] ->
+            N * 2;
+        _ ->
+            0
+    end.
 
 -spec three_of_a_kind(list()) -> integer().
 three_of_a_kind(DiceList) ->
-  case lists:sort(DiceList) of
-    [N, N, N, _, _] ->
-      N * 3;
-    [_, N, N, N, _] ->
-      N * 3;
-    [_, _, N, N, N] ->
-      N * 3;
-    _ ->
-      0
-  end.
+    case lists:sort(DiceList) of
+        [N, N, N, _, _] ->
+            N * 3;
+        [_, N, N, N, _] ->
+            N * 3;
+        [_, _, N, N, N] ->
+            N * 3;
+        _ ->
+            0
+    end.
 
 -spec four_of_a_kind(list()) -> integer().
 four_of_a_kind(DiceList) ->
-  case lists:sort(DiceList) of
-    [N, N, N, N, _] ->
-      4 * N;
-    [_, N, N, N, N] ->
-      4 * N;
-    _ ->
-      0
-  end.
+    case lists:sort(DiceList) of
+        [N, N, N, N, _] ->
+            4 * N;
+        [_, N, N, N, N] ->
+            4 * N;
+        _ ->
+            0
+    end.
 
 -spec two_pair(list()) -> integer().
 two_pair(DiceList) ->
-  case lists:sort(DiceList) of
-    [A, A, B, B, _] when A =/= B ->
-      A * 2 + B * 2;
-    [A, A, _, B, B] ->
-      A * 2 + B * 2;
-    [_, A, A, B, B] when A =/= B ->
-      A * 2 + B * 2;
-    _ ->
-      0
-  end.
+    case lists:sort(DiceList) of
+        [A, A, B, B, _] when A =/= B ->
+            A * 2 + B * 2;
+        [A, A, _, B, B] ->
+            A * 2 + B * 2;
+        [_, A, A, B, B] when A =/= B ->
+            A * 2 + B * 2;
+        _ ->
+            0
+    end.
 
 -spec small_straight(list()) -> integer().
 small_straight(DiceList) ->
-  case lists:sort(DiceList) of
-    [1, 2, 3, 4, 5] ->
-      15;
-    _ ->
-      0
-  end.
+    case lists:sort(DiceList) of
+        [1, 2, 3, 4, 5] ->
+            15;
+        _ ->
+            0
+    end.
 
 -spec large_straight(list()) -> integer().
 large_straight(DiceList) ->
-  case lists:sort(DiceList) of
-    [2, 3, 4, 5, 6] ->
-      20;
-    _ ->
-      0
-  end.
+    case lists:sort(DiceList) of
+        [2, 3, 4, 5, 6] ->
+            20;
+        _ ->
+            0
+    end.
 
 -spec full_house(list()) -> integer().
 full_house(DiceList) ->
-  case lists:sort(DiceList) of
-    [A, A, A, B, B] when A =/= B ->
-      lists:sum(DiceList);
-    [A, A, B, B, B] when A =/= B ->
-      lists:sum(DiceList);
-    _ ->
-      0
-  end.
+    case lists:sort(DiceList) of
+        [A, A, A, B, B] when A =/= B ->
+            lists:sum(DiceList);
+        [A, A, B, B, B] when A =/= B ->
+            lists:sum(DiceList);
+        _ ->
+            0
+    end.
 
 -spec chance(list()) -> integer().
 chance(DiceList) ->
-  lists:sum(DiceList).
+    lists:sum(DiceList).
 
 -spec yatzy(list()) -> integer().
 yatzy([N, N, N, N, N]) ->
-  50;
+    50;
 yatzy(_) ->
-  0.
+    0.
 
 -spec score_dice(atom(), list()) -> integer().
 score_dice(ones, DiceList) ->
-  upper(1, DiceList);
+    upper(1, DiceList);
 score_dice(twos, DiceList) ->
-  upper(2, DiceList);
+    upper(2, DiceList);
 score_dice(threes, DiceList) ->
-  upper(3, DiceList);
+    upper(3, DiceList);
 score_dice(fours, DiceList) ->
-  upper(4, DiceList);
+    upper(4, DiceList);
 score_dice(fives, DiceList) ->
-  upper(5, DiceList);
+    upper(5, DiceList);
 score_dice(sixes, DiceList) ->
-  upper(6, DiceList);
+    upper(6, DiceList);
 score_dice(one_pair, DiceList) ->
-  one_pair(DiceList);
+    one_pair(DiceList);
 score_dice(three_of_a_kind, DiceList) ->
-  three_of_a_kind(DiceList);
+    three_of_a_kind(DiceList);
 score_dice(four_of_a_kind, DiceList) ->
-  four_of_a_kind(DiceList);
+    four_of_a_kind(DiceList);
 score_dice(two_pair, DiceList) ->
-  two_pair(DiceList);
+    two_pair(DiceList);
 score_dice(full_house, DiceList) ->
-  full_house(DiceList);
+    full_house(DiceList);
 score_dice(small_straight, DiceList) ->
-  small_straight(DiceList);
+    small_straight(DiceList);
 score_dice(large_straight, DiceList) ->
-  large_straight(DiceList);
+    large_straight(DiceList);
 score_dice(chance, DiceList) ->
-  chance(DiceList);
+    chance(DiceList);
 score_dice(yatzy, DiceList) ->
-  yatzy(DiceList).
+    yatzy(DiceList).
